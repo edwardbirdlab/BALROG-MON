50b81fb8d65e9fb6993fe637119bd1f6d539a7bf
edwardbirdlab <edwardbirdlab@gmail.com>
2022-04-25 12:31:39 -0500

    edits containers in modules
    Adds option for use of local containers with using singularity

---
+++ Container_switcher.py
@@ -0,0 +1,50 @@
+import os
+
+BARA_Dir = '.'
+out_dir = './containers_down'
+
+search = 'container'
+containers = []
+if len(os.listdir(BARA_Dir + '/modules')) > 1:
+    for folder in os.listdir(BARA_Dir + '/modules'):
+        for mod in os.listdir(BARA_Dir + '/modules' + '/' + folder):
+            module = open(BARA_Dir + '/modules' + '/' + folder + '/' + mod, 'r')
+            for line in module:
+                if search in line:
+                    containers.append(line)
+            module.close()
+
+cut_containers = []
+for i in containers:
+    k = i.split()
+    con = k[1].strip("'")
+    cut_containers.append(con)
+
+
+containers = []
+
+for i in cut_containers:
+    if i in containers:
+        pass
+    else:
+        containers.append(i)
+
+new_file = ''
+
+for folder in os.listdir(BARA_Dir + '/modules'):
+        for mod in os.listdir(BARA_Dir + '/modules' + '/' + folder):
+            module = open(BARA_Dir + '/modules' + '/' + folder + '/' + mod, 'r')
+            for line in module:
+                for con in containers:
+                    if con in line:
+                        new_line = 'if (workflow.containerEngine == \'singularity\'){ container \'./containers_down/' + con + '.sif\' } else { container \'' + con + '\' }'
+                        new_file = new_file + new_line
+                    else:
+                        new_file = new_file + line
+            module.close()
+            module_edit = open(BARA_Dir + '/modules' + '/' + folder + '/' + mod, 'w')
+            module_edit.write(new_file)
+            module_eidt.close()
+
+
+
